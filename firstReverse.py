1f8f7e969a466c22b30e9d93111b0bd09d331444
System Administrator <root@Nabils-MacBook-Pro.local>
2018-08-05 14:37:03 -0600

    add firstreversal coding challenge solution

---
+++ firstReverse.py
@@ -0,0 +1,12 @@
+"""
+Using the Python language, have the function FirstReverse(str) take the str parameter being passed
+and return the string in reversed order. For example: if the input string is "Hello World and Coders"
+ then your program should return the string sredoC dna dlroW olleH.
+"""
+def FirstReverse(str):
+
+    # code goes here
+    return ''.join(reversed(str))
+
+
+print(FirstReverse("h333llLo"))
